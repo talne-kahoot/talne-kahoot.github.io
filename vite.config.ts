e113f8a0123f7b64ef7d587dc30c81c61b5bece7
Vladyslav Kyselov <vladyslav.kyselov@pitchbook.com>
2024-02-19 12:02:17 +0100

    add PWA to the kahoot

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
+import {VitePWA, VitePWAOptions} from "vite-plugin-pwa";
+
 
 // https://vitejs.dev/config/
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  registerType: "prompt",
+  includeAssets: ["512x512.png"],
+  manifest: {
+    name: "Talne Kahoot",
+    short_name: "Kahoot",
+    description: "Kahoot for Talne Church",
+    "icons": [
+      {"src": "/android-chrome-192x192.png", "sizes": "192x192", "type": "image/png"},
+      {"src": "/android-chrome-512x512.png", "sizes": "512x512", "type": "image/png"}
+    ],
+
+    theme_color: "#171717",
+    background_color: "#e8ebf2",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  }
+};
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugin)],
   base: '/'
 })
